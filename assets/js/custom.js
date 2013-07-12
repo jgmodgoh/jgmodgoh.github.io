4be01adbe384ced8b760d649f846f089a333d7a6
John Goh <jgmod@hotmail.com>
2013-07-12 16:06:02 -0400

    fixed quotes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,8 @@
 
    var quote_tag = $("#quote");
    var quotes = ['"Develop success from failures. Discouragement and failure are two of the surest stepping stones to success." - Dale Carnegie',
-                 '"Continuous effort - not strength or intelligence - is key to unlocking our potential." - Winston Churchill',
-                 '"We are all in the gutter, but some of us are looking at the stars." - Oscar Wilde'];
+                 '"Continuous effort - not strength or intelligence - is key to unlocking our potential." - Winston Churchill                  ',
+                 '"We are all in the gutter, but some of us are looking at the stars." - Oscar Wilde                                           '];
    var previous_quote = quote_tag.text();
    var current_quote = quotes[Math.floor(Math.random()*3)];
 
